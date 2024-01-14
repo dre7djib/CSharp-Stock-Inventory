18a45804f20d3cb4fdb9fa674ea08cf600440956
dre7djib <djibril.camara.pradier@gmail.com>
2024-01-15 00:53:58 +0100

    Structure Class

---
+++ class/Article.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Api {
+    public class Article {
+        public int Id { get; set;}
+        public string Title { get; set;}
+        public string Description { get; set;}
+        public int Quantity { get; set;}
+    }
+
+}
